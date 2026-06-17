cadc5914c6b0213b1758cc81553b4c300b5533ab
Rob Covert <clickhustles@gmail.com>
2026-06-17 13:44:48 -0400

    Database update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('company_id')->index()->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+
+            // Passwordless tracking tokens
+            $table->string('login_token', 64)->nullable()->unique();
+            $table->timestamp('token_expires_at')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
