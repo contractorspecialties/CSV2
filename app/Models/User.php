07237b7f7a03a6c71412c07e3ce2a6913e71eb5d
Rob Covert <clickhustles@gmail.com>
2026-06-17 13:47:39 -0400

    User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,34 +2,47 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use SoftDeletes;
+    use HasFactory, Notifiable;
 
-    protected $guarded = [];
-
-    protected $hidden = [
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'company_id',
+        'first_name',
+        'last_name',
+        'email',
         'login_token',
+        'token_expires_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
         'remember_token',
     ];
 
-    public function __construct(array $attributes = [])
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        parent::__construct($attributes);
-
-        $defaultConnection = config('database.default', 'sqlite');
-        $configuredPrefix = config("database.connections.{$defaultConnection}.prefix");
-        $prefix = !empty($configuredPrefix) ? $configuredPrefix : 'sc_';
-
-        $this->setTable($prefix . 'users');
-    }
-
-    public function company(): BelongsTo
-    {
-        return $this->belongsTo(Company::class);
+        return [
+            'email_verified_at' => 'datetime',
+            'token_expires_at' => 'datetime',
+        ];
     }
 }
