ea0bcb9000145cf8637041d6611e68ec46e07096
Rob Covert <clickhustles@gmail.com>
2026-06-26 12:35:20 -0400

    Pricebook Safety

--- app/Models/PricebookItem.php
+++ app/Models/PricebookItem.php
@@ -2,16 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PricebookItem extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, BelongsToTenant;
 
     protected $guarded = [];
 
+    /**
+     * Re-bind core lifecycle constructor footprints to track prefix sequences.
+     */
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
@@ -23,6 +27,9 @@ class PricebookItem extends Model
         $this->setTable($prefix . 'pricebook_items');
     }
 
+    /**
+     * Map workspace records straight back up to parent company corporate anchors.
+     */
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
